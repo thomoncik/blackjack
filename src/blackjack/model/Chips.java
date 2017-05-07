269e358c35cff6d6bdc6785547239bcd0e47068f
matkaczmarek <mat_kaczmarek@outlook.com>
2017-05-07 22:30:58 +0200

    Little Changes

--- src/blackjack/model/Chips.java
+++ src/blackjack/model/Chips.java
@@ -5,19 +5,19 @@ import javafx.util.Pair;
 /**
  * Created by Jan on 5/7/2017.
  */
-public class Token {
+public class Chips {
 
     private Pair[] values = new Pair[]{
-            new Pair<>(5, Token.Color.GRAY),
-            new Pair<>(25, Token.Color.RED),
-            new Pair<>(50, Token.Color.GREEN),
-            new Pair<>(100, Token.Color.BLACK),
-            new Pair<>(500, Token.Color.BLUE),
+            new Pair<>(5, Chips.Color.GRAY),
+            new Pair<>(25, Chips.Color.RED),
+            new Pair<>(50, Chips.Color.GREEN),
+            new Pair<>(100, Chips.Color.BLACK),
+            new Pair<>(500, Chips.Color.BLUE),
     };
     private int value;
     private Color color;
 
-    public Token(int value) {
+    public Chips(int value) {
         this.value = value;
         for (Pair p : values) {
             if (p.getKey() == new Integer(value)) {
