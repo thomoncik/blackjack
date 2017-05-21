5d8345ae6dd3f755f0487b48a9691a674ee64541
Jan <jan.meleche@op.pl>
2017-05-21 13:35:05 +0200

    Added method getNextCard and ArrayList<Integer> randomOrderOfCards, which will be randomly sorted. From it we will get next index in our array Card[] cards ( using added int nextCard meaning index in our random shuffled ArrayList ).

--- src/blackjack/model/Deck.java
+++ src/blackjack/model/Deck.java
@@ -1,5 +1,8 @@
 package blackjack.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
 
 /**
@@ -7,20 +10,35 @@ import java.util.Random;
  */
 class Deck {
     private Card[] cards;
+    private ArrayList<Integer> randomOrderOfCards;
+    private int nextCard;
     public static final int DECK_SIZE = Card.Rank.values().length * Card.Suit.values().length;
 
     Deck() {
         cards = new Card[DECK_SIZE];
+        randomOrderOfCards = new ArrayList<>();
         int i = 0;
         for (Card.Rank rank : Card.Rank.values()) {
             for (Card.Suit suit : Card.Suit.values()) {
+                randomOrderOfCards.add(i);
                 cards[i++] = new Card(suit, rank);
             }
         }
+        Collections.shuffle(randomOrderOfCards);
+        nextCard = 0;
     }
 
     // @TODO: Fix
+    Card getNextCard() {
+        return cards[ randomOrderOfCards.get(nextCard++) ];
+    }
     Card getRandomCard() {
         return cards[new Random().nextInt(DECK_SIZE)];
+    }
+
+    public static void main(String[] args){
+        // pseudo unit tests checking getting random card
+        Deck deck = new Deck();
+        for( int i = 0 ; i < DECK_SIZE; i++ ) System.out.println(deck.getNextCard());
     }
 }
