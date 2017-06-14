3b6facc781fb0ffcb441b95a0caa4ce73de32a62
thomoncik <tomasz_pl@interia.pl>
2017-06-14 13:56:36 +0200

    Test fixes

--- test/blackjack/model/DealerTest.java
+++ test/blackjack/model/DealerTest.java
@@ -13,6 +13,9 @@ public class DealerTest {
     public void canTakeCard() throws Exception {
 //        TableProvider aces = new TableProvider("aces");
         TableProvider takeCard = new TableProvider("takeCard");
+        takeCard.getPlayer().setDuringRound(true);
+        takeCard.getDealer().begin();
+        takeCard.getPlayer().hit();
 
 //        assertFalse(aces.getDealer().canTakeCard());
         assertTrue(takeCard.getDealer().canTakeCard());
@@ -29,6 +32,10 @@ public class DealerTest {
     @Test
     public void gotBlackjack() throws Exception {
         TableProvider table = new TableProvider("blackjack");
+        table.getPlayer().setDuringRound(true);
+        table.getDealer().begin();
+        table.getPlayer().hit();
+
         assertTrue(table.getDealer().gotBlackjack());
     }
 
@@ -40,6 +47,13 @@ public class DealerTest {
     public void gotAceOnFace() throws Exception {
         TableProvider aces = new TableProvider("aces");
         TableProvider falseAces = new TableProvider("falseAces");
+        aces.getPlayer().setDuringRound(true);
+        aces.getDealer().begin();
+        aces.getPlayer().hit();
+
+        falseAces.getPlayer().setDuringRound(true);
+        falseAces.getDealer().begin();
+        falseAces.getPlayer().hit();
 
         assertTrue(aces.getDealer().gotAceOnFace());
         assertFalse(falseAces.getDealer().gotAceOnFace());
@@ -48,6 +62,9 @@ public class DealerTest {
     @Test
     public void clearMyHand() throws Exception {
         Table table = new Table();
+        table.getPlayer().setDuringRound(true);
+        table.getDealer().begin();
+        table.getPlayer().hit();
 
         assertTrue(table.getDealer().getValue() > 0);
 
--- test/blackjack/model/PlayerTest.java
+++ test/blackjack/model/PlayerTest.java
@@ -11,6 +11,10 @@ public class PlayerTest {
     @Test
     public void canSplit() throws Exception {
         TableProvider table = new TableProvider("split");
+        table.getPlayer().setDuringRound(true);
+        table.getDealer().begin();
+        table.getPlayer().hit();
+
         assertFalse(table.getPlayer().canSplit()); // for split player needs 2 cards
 
         table.getPlayer().hit();
@@ -23,6 +27,10 @@ public class PlayerTest {
         assertFalse(table.getPlayer().canSplit()); // exactly 2 cards
 
         table = new TableProvider("falseSplit");
+        table.getPlayer().setDuringRound(true);
+        table.getDealer().begin();
+        table.getPlayer().hit();
+
         table.getPlayer().hit();
         assertFalse(table.getPlayer().canSplit());
     }
@@ -30,9 +38,17 @@ public class PlayerTest {
     @Test
     public void canDoubleDown() throws Exception {
         TableProvider table = new TableProvider("doubleDown");
+        table.getPlayer().setDuringRound(true);
+        table.getDealer().begin();
+        table.getPlayer().hit();
+
         assertTrue(table.getPlayer().canDoubleDown());
 
         table = new TableProvider("doubleDown");
+        table.getPlayer().setDuringRound(true);
+        table.getDealer().begin();
+        table.getPlayer().hit();
+
         table.getPlayer().setBet(100);
         table.getPlayer().setBank(50);
         assertFalse(table.getPlayer().canDoubleDown());
@@ -43,6 +59,14 @@ public class PlayerTest {
         TableProvider insurance = new TableProvider("insurance");
         TableProvider falseInsurance = new TableProvider("falseInsurance");
 
+        insurance.getPlayer().setDuringRound(true);
+        insurance.getDealer().begin();
+        insurance.getPlayer().hit();
+
+        falseInsurance.getPlayer().setDuringRound(true);
+        falseInsurance.getDealer().begin();
+        falseInsurance.getPlayer().hit();
+
         assertTrue(insurance.canBuyInsurance());
         assertFalse(falseInsurance.canBuyInsurance());
 
@@ -51,6 +75,10 @@ public class PlayerTest {
         assertFalse(insurance.canBuyInsurance());
 
         insurance = new TableProvider("insurance");
+
+        insurance.getPlayer().setDuringRound(true);
+        insurance.getDealer().begin();
+        insurance.getPlayer().hit();
 
         assertTrue(insurance.canBuyInsurance());
 
@@ -63,6 +91,9 @@ public class PlayerTest {
     @Test
     public void clearMyHand() throws Exception {
         Table table = new Table();
+        table.getPlayer().setDuringRound(true);
+        table.getDealer().begin();
+        table.getPlayer().hit();
 
         assertTrue(table.getPlayer().getValue() > 0);
 
@@ -74,6 +105,9 @@ public class PlayerTest {
     @Test
     public void myValue() throws Exception {
         TableProvider aces = new TableProvider("aces");
+        aces.getPlayer().setDuringRound(true);
+        aces.getDealer().begin();
+        aces.getPlayer().hit();
 
         aces.getPlayer().hit();
         assertEquals(12, aces.getPlayer().getValue());
